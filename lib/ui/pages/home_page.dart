400bbc9074726c98615075672afb9cf1c8311f6a
WaleedMob <116899879+WaleedMob@users.noreply.github.com>
2023-02-16 13:46:59 +0200

    first commit

---
+++ lib/ui/pages/home_page.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: avoid_unnecessary_containers
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: const Text('g؟',style: TextStyle(fontSize: 50),),
+      ),
+    );
+  }
+}
